8270119350d4e91f525f790b4fe9760f54568901
Kylie McClain <kylie@somas.is>
2022-11-16 12:04:38 -0500

    manifest: o sin ala e nimi mi

--- manifest.sh
+++ manifest.sh
@@ -8,7 +8,7 @@ jq -n \
     --arg name "toki pona" \
     --arg version "${version}" \
     --arg description "tenpo pi toki pona li lon a!" \
-    --arg author "cominixo, jan Juli / Kylie McClain" \
+    --arg author "cominixo, jan Kali / Kylie McClain" \
     --arg license "MIT" \
     --arg website "https://github.com/somasis/discord-tokipona" \
     '$ARGS.named'
